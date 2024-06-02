8a77a025a5312725db9d6d1c9df00bb2c1ae15b4
gokul-elumalai <src.mv1f3@aleeas.com>
2024-06-02 22:45:33 +0530

    Add Pricing strategy use case for Strategy pattern implementation

---
+++ src/main/java/design_patterns/strategy/refactor_price_calculation/PricingStrategy.java
@@ -0,0 +1,5 @@
+package main.java.design_patterns.strategy.refactor_price_calculation;
+
+public interface PricingStrategy {
+    PricingType supportsType();
+}
---
+++ src/main/java/design_patterns/strategy/refactor_price_calculation/PricingType.java
@@ -0,0 +1,7 @@
+package main.java.design_patterns.strategy.refactor_price_calculation;
+
+public enum PricingType {
+    TIME_BASED,
+    DISTANCE_BASED,
+    SURGE
+}
